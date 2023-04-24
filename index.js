fbb34581e3b2e9bae86cf9b798239bcce77f109f
Sirius Black <mariia.beliaev@gmail.com>
2023-04-24 23:16:07 +0300

    added twoSum function which works wrong

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ function crawlTree(array) {
 }
 
 // Код для второго задания напиши в этой функции
-function twoSum() {}
+
+function twoSum(arr, target) {
+  let indexes = [];
+  arr.map((a, i) => {
+    arr.map((b, j) => {
+      if (i !== j && a + b === target) {
+        indexes.push(i, j);
+      }
+    });
+  });
+  return indexes;
+}
+
+const arr = [3, 2, 4];
+const target = 6;
+console.log(twoSum(arr, target));
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
