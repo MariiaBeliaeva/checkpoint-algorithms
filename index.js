c9c6b30ad4f7dfd48a2bca2e0a2e32484bb0620f
Sirius Black <mariia.beliaev@gmail.com>
2023-04-24 21:51:57 +0300

    added crawlTree solution

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Код для первого задания напиши в этой функции
-function crawlTree() {}
+
+let treeToString = "";
+
+function crawlTree(array) {
+  if (Array.isArray(array)) {
+    for (let i = 0; i < array.length; i++) {
+      crawlTree(array[i]);
+    }
+  } else if (typeof array === "string") {
+    treeToString += array;
+  }
+  return treeToString;
+}
 
 // Код для второго задания напиши в этой функции
 function twoSum() {}
