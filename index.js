3faa79e02dfbd885ef5947592d6819aa9a9fdc0e
Sirius Black <mariia.beliaev@gmail.com>
2023-04-25 13:30:10 +0300

    changed twoSum for recursion

--- index.js
+++ index.js
@@ -14,16 +14,29 @@ function crawlTree(array) {
 
 // Код для второго задания напиши в этой функции
 
-function twoSum(arr, target) {
-  let indexes = [];
-  for (let i = 0; i <= arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] == target) {
-        indexes.push(i, j);
-      }
-    }
+// function twoSum(arr, target) {
+//   let indexes = [];
+//   for (let i = 0; i <= arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] == target) {
+//         indexes.push(i, j);
+//       }
+//     }
+//   }
+//   return indexes;
+// }
+
+function twoSum(arr, target, i = 0, j = 1, indexes = []) {
+  if (i >= arr.length) {
+    return indexes;
   }
-  return indexes;
+  if (j >= arr.length) {
+    return twoSum(arr, target, i + 1, i + 2, indexes);
+  }
+  if (arr[i] + arr[j] === target) {
+    indexes.push(i, j);
+  }
+  return twoSum(arr, target, i, j + 1, indexes);
 }
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
